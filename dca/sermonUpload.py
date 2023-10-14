f1c22e2202630037123d4d0a4a85a96c4ca0baa2
heyt0pe <akinthope@gmail.com>
2023-10-14 14:11:51 +0100

    dca sermon upload

--- dca/sermonUpload.py
+++ dca/sermonUpload.py
@@ -1,9 +1,21 @@
 sermons = [
     {
-        'driveLink': 'https://drive.google.com/file/d/1vU0XaaKlLNJtKVa3PAuE1OGEGb_MMU2F/view?usp=sharing',
-        'preacher': 'Dcn. Tobi Akapo',
-        'sermonTitle': 'The Effects of Light',
-        'sermonDate': '30th January, 2022',
+        'driveLink': 'https://drive.google.com/file/d/1IrIYPe9bYBIaBVfOWQDc3bB92lOFeVLU/view?usp=drive_link',
+        'preacher': 'Rev. Olubusayo Folarin',
+        'sermonTitle': 'Need for Speed (Delay)',
+        'sermonDate': '2023-09-17 00:00:00',
+    },
+    {
+        'driveLink': 'https://drive.google.com/file/d/1wSAx9JKcWJbge6VDgDF7bq9ykPxQ6pbI/view?usp=sharing',
+        'preacher': 'Rev. Olubusayo Folarin',
+        'sermonTitle': 'Heaven on Earth',
+        'sermonDate': '2023-10-01 00:00:00',
+    },
+    {
+        'driveLink': 'https://drive.google.com/file/d/1IFos5bkcJZMKxInMEYMwzfdRHIXj8wIi/view?usp=sharing',
+        'preacher': 'Rev. Olubusayo Folarin',
+        'sermonTitle': 'The Power of Forgiveness',
+        'sermonDate': '2023-10-08 00:00:00',
     },
 ]
 
@@ -16,9 +28,10 @@ def convertToExportLink(driveLink):
     return driveLink
 
 
-sqlScript = "INSERT INTO `sermons` (`entryIndex`, `driveLink`, `preacher`, `sermonTitle`, `sermonDate`) VALUES"
-entryIndex = 34
+sqlScript = "INSERT INTO `sermons` (`sermonId`, `sermon`, `preacher`, `title`, `date`) VALUES"
+entryIndex = 87
 for sermon in sermons:
+    entryIndex += 1
     driveLink = sermon['driveLink']
     driveLink = convertToExportLink(driveLink)
     sermon['driveLink'] = 'http://docs.google.com/uc?export=open&id={}'.format(
@@ -26,7 +39,7 @@ for sermon in sermons:
     sqlScript += "('{}', '{}', '{}', '{}', '{}'),".format(
         entryIndex, sermon['driveLink'], sermon['preacher'], sermon['sermonTitle'], sermon['sermonDate'],
     )
-    entryIndex += 1
+    
 
 sqlScript = sqlScript[:len(sqlScript) - 1]
 sqlScript += ';'
